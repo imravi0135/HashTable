00de1b8c2567f40ba8e72db69fd2804b3b99454f
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-30 06:44:28 +0530

    Welcome To The HashTable Problems

---
+++ HashTable/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class LinkedList<K, V> where K : IComparable
+    {
+        public MyMapNode<K, V> head;
+        public MyMapNode<K, V> tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+        internal MyMapNode<K, V> Search(K key)
+        {
+            MyMapNode<K, V> temp = head;
+            while (temp != null)
+            {
+                if (temp.key.Equals(key))
+                    return temp;
+                else
+                {
+                    temp = temp.next;
+                }
+            }
+            return null;
+        }
+        public void Append(MyMapNode<K, V> node)
+        {
+            if (head == null && tail == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                tail.next = node;
+                tail = node;
+            }
+        }
+    }
+}
